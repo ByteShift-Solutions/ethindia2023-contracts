f44797c76efb0880d7860e87f81e3a541c2d2fd4
rachit2501 <rac.sri25@gmail.com>
2023-12-09 21:12:37 +0530

    function signatures and implementations for core

--- contracts/src/Core.sol
+++ contracts/src/Core.sol
@@ -2,13 +2,27 @@
 pragma solidity ^0.8.3;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Core is ERC721("score", "SCR") {
-    uint256 id = 1;
-    mapping(uint256 => address) connectorId;
-    mapping(address => bool) user;
-    mapping(address => uint256) protocolToConnector;
 
+    struct User {
+        uint256 id;
+        uint256 nativeSecurity;
+        mapping(address => uint256) erc20Security;
+    }
+
+    uint256 id = 1; // rename
+    uint256 userId = 1; // rename
+    mapping(uint256 => address) connectorId;
+    mapping(address => User) userInfo;
+    mapping(address => uint256) protocolToConnector;
+    mapping(address => bool) whitelistedTokens;
+
+    modifier onlyRegisteredUser(address addr) {
+        require(userInfo[addr].id != 0, "User not registered");
+        _;
+    }
     function aggregateScores(address user, uint256[] calldata ids) public {
         uint length = ids.length;
         for (uint i = 0; i < length; i++) {
@@ -16,11 +30,25 @@ contract Core is ERC721("score", "SCR") {
         }
     }
 
-    function userRegistration() public {}
+    function userRegistration() public {
+        require(userInfo[msg.sender].id == 0, "already registered");
+        User storage newUser = userInfo[msg.sender];
+        newUser.id = userId++;
+    }
 
-    function depositSecurity() public {}
+    function depositSecurity() onlyRegisteredUser(msg.sender) public payable {
+        uint256 value = msg.value;
+        userInfo[msg.sender].nativeSecurity = value;
+    }
 
-    function depositSecurityWithERC20() public {}
+    function depositSecurityWithERC20(uint256 amount, address tokenAddress) public {
+        IERC20(tokenAddress).transferFrom( msg.sender, address(this), amount);
+        userInfo[msg.sender].erc20Security[tokenAddress] = amount;
+    }
+
+    function whitelistAddress(address tokenAddress) external {
+        whitelistedTokens[tokenAddress] = true;
+    }
 
     function registerConnector(address connectorAddr) public {
         // TODO: implement interface sanity check here
