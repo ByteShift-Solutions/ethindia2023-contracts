02d05e54c6ba7a5632c84cb06bc29f4eaadc14a2
rachit2501 <rac.sri25@gmail.com>
2023-12-09 19:29:24 +0530

    sudo definations for connectors

---
+++ contracts/src/Connector.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
+
+// extens interface
+contract Connector {
+    // code to define the equation concerned
+    // code to interact with the generator library
+    // code to update the scores
+    // interaction interface for the core ( aggregator )
+}
---
+++ contracts/src/Core.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Core is ERC721 {
+
+    uint256 id = 1;
+    mapping(uint256 => address) connectorId;
+    mapping(address => bool) user;
+    mapping(address => uint256) protocolToConnector;
+
+    function aggregateScores(address user, uint256[] calldata ids) public {
+        uint length = ids.length;
+        for(uint i=0; i<length; i++){
+            // TODO: need interface
+        }
+    }
+    
+    function userRegistration() public {}
+
+    function depositSecurity() public {}
+
+    function depositSecurityWithERC20() public {}
+
+    function registerConnector(address connectorAddr) public {
+        // TODO: implement interface sanity check here 
+        connectorId[id++] = connectorAddr;
+    }
+
+    function subscribeProtocolToConnector(address protocolAddress,uint256 targetConnectorId) public {
+        require(targetConnectorId == protocolToConnector[protocolAddress],"Invalid connector");
+        protocolToConnector[protocolAddress] = 0;
+    }
+
+    function unsubscribeProtocolFromConnector(address protocolAddress, uint256 targetConnectorId) public {
+        protocolToConnector[protocolAddress] = targetConnectorId;
+    }
+}
